d6315020ea486d958e2e6785ecd0b750a85f58d0
Cagan Cerkez <cagan.cerkez@tapaas.com>
2020-06-22 17:49:57 +0300

    add clear session button on error page

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,4 @@
-clearSession() {
+function clearSession() {
     var url = window.location.href; 
     var redirectURL = url.substring(url.indexOf("?")+1).split("&")[0];
     window.location.href = "http://" + window.location.hostname + "/logout?" + redirectURL;
