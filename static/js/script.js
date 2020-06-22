f5007af6046ac0bd2f65803e3195d3f63b572682
Cagan Cerkez <cagan.cerkez@tapaas.com>
2020-06-22 18:47:36 +0300

    add clear session button on error page

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,14 @@
 function clearSession() {
     var url = window.location.href; 
     var redirectURL = url.substring(url.indexOf("?")+1).split("&")[0];
-    window.location.href = "https://" + window.location.hostname + "/logout?" + redirectURL;
+    console.log("https://" + window.location.hostname + "/logout?" + redirectURL);
+    //window.location.href = "https://" + window.location.hostname + "/logout?" + redirectURL;
+
+    // Logout silently without a redirect
+    (function(){
+        (new Image()).src = "https://testvouch.tapaas.com/logout";
+        setTimeout(function(){ 
+            window.location.href = redirectURL.split("=")[1]; 
+        }, 1500); // Wait to get cookie cleared before redirecting
+    })();   
 }
