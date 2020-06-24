5190159c15cecede456ec261c1c109da48b69997
Cagan Cerkez <cagan.cerkez@tapaas.com>
2020-06-24 19:27:24 +0300

    do not redirect if no url= param

--- static/js/script.js
+++ static/js/script.js
@@ -1,43 +1,48 @@
 var errorMsg = document.getElementById("errorMsg").textContent;
+var clearButton = document.getElementById("clearButton");
 
 if (errorMsg.includes("Forbidden")) {
     document.getElementById('withoutButton').style.display = 'block';
     //Clear vouch/okta sessions
     setTimeout(function () {
         //Logout URL on dev "https://testvouch.tapaas.com/logout";
-        (function () {
-            (new Image()).src = "https://" + window.location.hostname + "/logout";
-        })();
-        console.log("Vouch session cleared: " + "https://" + window.location.hostname + "/logout");
-        
-        //Okta signout URL on dev ""https://id.tapaas.com/login/signout?fromURI=https://demo15.tapaas.com/dashboard"";
-        (function () {
-            (new Image()).src = "https://id.tapaas.com/login/signout";
-        })();
-        console.log("IDP session cleared: " + "https://id.tapaas.com/login/signout");
-
-    }, 500); // Wait a bit for the cookie to get set before clearing it
+        clearSessions();
+    }, 500); // Add a short delay before clearing cookie
 } else {
     document.getElementById('showButton').style.display = 'block';
 }
 
-function clearSession() {
-    var url = window.location.href;
-    var redirectURL = url.substring(url.indexOf("?") + 1).split("&")[0];
-    //window.location.href = "https://" + window.location.hostname + "/logout?" + redirectURL;
+function clearSessionButton() {
+    clearSessions();
+    clearButton.innerHTML = "Session Cleared!"
 
+    // Expected URL in address bar: https://testvouch.tapaas.com/login?url=https://demo.tapaas.com/dashboard/&vouch-failcount=&X-Vouch-Token=&error=
+    // URL after login page timeout: https://testvouch.tapaas.com/auth?code=dfdsfdsfmAdRRK (no url= parameter)
+    var url = window.location.href;
+    var urlParamIndex = url.indexOf("url=");
+
+    if (urlParamIndex > 0) {
+        var redirectURL = url.substring(urlParamIndex + 1).split("&")[0];
+        //window.location.href = "https://" + window.location.hostname + "/logout?" + redirectURL;
+
+        // Do not attempt to redirect if the URL does not include a url= parameter to a tapaas domain
+        if (redirectURL.includes("tapaas.com")) { 
+            setTimeout(function () {
+                window.location.href = redirectURL.split("=")[1]; // Redirect to the url= parameter in the address bar
+                console.log("redirecting to: https://" + window.location.hostname + "/logout?" + redirectURL);     
+            }, 1000); // Wait to get cookie cleared before redirecting
+        }
+    }
+}
+
+function clearSessions() {
     (function () {
         (new Image()).src = "https://id.tapaas.com/login/signout";
     })();
     console.log("IDP session cleared: " + "https://id.tapaas.com/login/signout");
 
-    // First logout silently without a redirect
-    // Button click inherently has a delay, so no need to execute /logout inside a settimeout function like above
     (function () {
         (new Image()).src = "https://" + window.location.hostname + "/logout";
-        setTimeout(function () {
-            window.location.href = redirectURL.split("=")[1]; // Redirect to the url= parameter in the address bar
-            console.log("redirecting to: https://" + window.location.hostname + "/logout?" + redirectURL);
-        }, 1000); // Wait to get cookie cleared before redirecting
     })();
+    console.log("Vouch session cleared: " + "https://" + window.location.hostname + "/logout");
 }
