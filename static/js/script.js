1febb6c889cadb8f098f356dc71be51932a5cc38
Cagan Cerkez <cagan.cerkez@tapaas.com>
2020-07-13 09:15:44 +0300

    redirect to id site w/out url param

--- static/js/script.js
+++ static/js/script.js
@@ -5,34 +5,24 @@ if (errorMsg.includes("Forbidden")) {
     document.getElementById('forbidden').style.display = 'block';
     //Clear vouch/okta sessions
     setTimeout(function () {        
-        clearSessions();
+        clearSessions(false); // Do not redirect to id.tapaas.com if account is not permissioned
     }, 500); // Add a short delay before clearing cookie
 } else {
     document.getElementById('showButton').style.display = 'block';
 }
 
-function clearSessionButton() {
+function clearSessionButton(redirect) {
     clearSessions();
     clearButton.innerHTML = "Session Cleared!"
 
     // Expected URL in address bar: https://testvouch.tapaas.com/login?url=https://demo.tapaas.com/dashboard/&vouch-failcount=&X-Vouch-Token=&error=
     // URL after login page timeout: https://testvouch.tapaas.com/auth?code=dfdsfdsfmAdRRK (no url= parameter)
-    var url = window.location.href;
-    var urlParamIndex = url.indexOf("url=");
-
-    if (urlParamIndex > 0) {
-        //var redirectURL = url.substring(urlParamIndex + 1).split("&")[0];
+    if (redirect) {
         var redirectURL = "url=https://id.tapaas.com"
-        //window.location.href = "https://" + window.location.hostname + "/logout?" + redirectURL;
-
-        // Do not attempt to redirect if the URL does not include a url= parameter to a tapaas domain
-        if (redirectURL.includes("tapaas.com")) { 
-            setTimeout(function () {
-                window.location.href = redirectURL.split("=")[1]; // Redirect to the url= parameter in the address bar
-                console.log("redirecting to: https://" + window.location.hostname + "/logout?" + redirectURL);     
-            }, 1000); // Wait to get cookie cleared before redirecting
-        }
-    }
+        setTimeout(function () {
+            console.log("redirecting to: https://" + window.location.hostname + "/logout?" + redirectURL);     
+        }, 1000); // Wait to get cookie cleared before redirecting
+    }    
 }
 
 function clearSessions() {
